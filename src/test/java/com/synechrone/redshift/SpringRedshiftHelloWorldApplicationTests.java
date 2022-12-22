af0c28867947eb2ca9c696236535754ac9f3251c
riteshkdalvi <58825348+riteshkdalvi@users.noreply.github.com>
2022-12-22 17:09:57 +0530

    Add files via upload

---
+++ src/test/java/com/synechrone/redshift/SpringRedshiftHelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.synechrone.redshift;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRedshiftHelloWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
